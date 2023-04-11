d50768da8657aaa5e09f03e51bc66bc9b115325b
viettrungIT3 <viettrungcntt03@gmail.com>
2023-04-12 01:09:48 +0700

    add feature footer

--- src/application/controllers/Dashboard.php
+++ src/application/controllers/Dashboard.php
@@ -10,6 +10,17 @@ class Dashboard extends MY_Controller{
     public function index(){
 		
 		return $this
+        ->set("data_source", base_url(""))
+		->set_full_layout(TRUE)
+		->set_body_class("dashboard-listing")
+		->set_page_title("dashboard_listing")
+		->set_main_template("index")
+		->render();
+    }
+
+    public function team(){
+		
+		return $this
         ->set("data_source", base_url("/order/search"))
 		->set("download_url", get_csv_url())
 		->set("download_filename", "Shop order transaction report")
---
+++ src/public/views/index.php
@@ -0,0 +1,10 @@
+<div class="wrapper main-page-wrapper">
+
+    <div class="wrapper-inner">
+        
+        abc
+
+
+    </div><!-- wrapper inner -->
+
+</div><!-- end of main page wrapper -->
