3abb9d602e5c0309afa5ef7f2c61b4694779d96c
viettrungIT3 <viettrungcntt03@gmail.com>
2023-04-14 22:39:53 +0700

    delete set timer

--- src/public/views/common/footer.php
+++ src/public/views/common/footer.php
@@ -2,40 +2,6 @@
 
 </div><!--end of page wrapper-->
 
-<?php if (!isset($params['timer']) || $params['timer'] === TRUE) : ?>
-	<div class="wrapper logout-wrapper">
-		<div class="logout-wrapper-inner">
-
-			<div class="logout-container">
-
-				<div class="logout-container-inner">
-
-					<h2>You will be logged out soon</h2>
-
-					<div class="logout-content">
-
-						<div class="logout-content-inner">
-
-							Auto logout is happening in <span class='seconds-count'></span> seconds.
-
-						</div>
-
-					</div>
-
-					<div class="logout-footer">
-						<a href="#" id='btn-continue'>I'm still here</a>
-						<a href="#" id='btn-logout'>Logout</a>
-					</div>
-
-				</div>
-
-			</div>
-
-		</div>
-	</div>
-
-<?php endif; ?>
-
 
 <?php
 $currentPage = basename($_SERVER['REQUEST_URI']);
