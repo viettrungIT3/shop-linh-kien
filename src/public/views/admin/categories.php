9ec0ec20776e6494b703d37db0d3a1a4ad2b2354
viettrungIT3 <viettrungcntt03@gmail.com>
2023-04-28 03:23:45 +0700

    update UI Category: add modal(add, edit, delete)

--- src/public/views/admin/categories.php
+++ src/public/views/admin/categories.php
@@ -1,14 +1,20 @@
 ﻿<!-- Start container-fluid -->
 
+<style id="css-header-bar">
+    <?= load_css("modal-admin") ?>
+</style>
+
+
 <?php $categories = $params["categories"]['data'] ?>
 <div class="container-fluid">
 
     <!-- start  -->
     <div class="row">
-        <div class="col-12">
-            <div>
-                <h4 class="header-title mb-3">Categories</h4>
-            </div>
+        <div class="col-sm-9">
+            <h2 class="header-title mb-3 fs-2" style="font-size: 2rem;">Categories</h2>
+        </div>
+        <div class="col-sm-3 text-left">
+            <a href="#addCategoryModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Category</span></a>
         </div>
     </div>
     <!-- end row -->
@@ -37,8 +43,12 @@
                                 <td><?= $category->description ?></td>
                                 <td>
                                     <button type="button" class="btn btn-primary"><i class="far fa-eye"></i></button>
-                                    <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
-                                    <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+                                    <button type="button" class="btn btn-success" href="#editCategoryModal" class="edit" data-toggle="modal" style="color: #fff;">
+                                        <i class="fas fa-edit" data-toggle="tooltip" title="Edit"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-danger" href="#deleteCategoryModal" class="delete" data-toggle="modal" style="color: #fff;">
+                                        <i class="far fa-trash-alt" data-toggle="tooltip" title="Delete"></i>
+                                    </button>
                                 </td>
                             </tr>
                         <?php } ?>
@@ -51,6 +61,130 @@
         </div>
     </div>
     <!-- end row -->
+
+    <!-- ===modal=== -->
+    <!-- Add Modal HTML -->
+    <div id="addCategoryModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form>
+                    <div class="modal-header">
+                        <h4 class="modal-title">Add Category</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea class="form-control" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Phone</label>
+                            <input type="text" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+                        <input type="submit" class="btn btn-success" value="Add">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Edit Modal HTML -->
+    <div id="editCategoryModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form>
+                    <div class="modal-header">
+                        <h4 class="modal-title">Edit Category</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea class="form-control" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Phone</label>
+                            <input type="text" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+                        <input type="submit" class="btn btn-info" value="Save">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Delete Modal HTML -->
+    <div id="deleteCategoryModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form>
+                    <div class="modal-header">
+                        <h4 class="modal-title">Delete Category</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Are you sure you want to delete these Records?</p>
+                        <p class="text-warning"><small>This action cannot be undone.</small></p>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
+                        <input type="submit" class="btn btn-danger" value="Delete">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- partial -->
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+    <script src="">
+        $(document).ready(function() {
+            // Activate tooltip
+            $('[data-toggle="tooltip"]').tooltip();
+
+            // Select/Deselect checkboxes
+            var checkbox = $('table tbody input[type="checkbox"]');
+            $("#selectAll").click(function() {
+                if (this.checked) {
+                    checkbox.each(function() {
+                        this.checked = true;
+                    });
+                } else {
+                    checkbox.each(function() {
+                        this.checked = false;
+                    });
+                }
+            });
+            checkbox.click(function() {
+                if (!this.checked) {
+                    $("#selectAll").prop("checked", false);
+                }
+            });
+        });
+    </script>
+
+    <!-- end modal -->
 
 </div>
 <!-- end container-fluid -->
